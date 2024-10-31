a80ac6dc4f34eb511e249b16d49d55bffeb92997
Jean Machuca <correojean@gmail.com>
2024-10-31 16:42:00 -0300

    remove node_modules
    remove package-lock
    
    add gitignore
    
    integrate tailwindcss
    
    theme
    
    theme
    
    .gitignore
    
    init
    
    sass

--- src/js/init.js
+++ src/js/init.js
@@ -1,11 +1,10 @@
-import * as QCObjects from "https://unpkg.com/qcobjects@2.4.107-ts";
+import * as QCObjects from "qcobjects";
 import "./components/nav.js";
 
 RegisterWidgets("header-component", "home-component", 
     "features-component", "about-component", 
     "contact-component", "footer-component");
 
-Package("com.qcobjects.components", [Nav]);
 
 
 CONFIG.set("sourceType", "module");
